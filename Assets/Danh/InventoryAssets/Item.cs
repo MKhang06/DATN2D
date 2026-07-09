427dfee309d5191b7d73555186c460ba942d3638
MKhang06 <phamminhkhang081120@gmail.com>
2026-07-09 14:55:57 +0700

    xoa

--- Assets/Danh/InventoryAssets/Item.cs
+++ Assets/Danh/InventoryAssets/Item.cs
@@ -1,10 +1,10 @@
 
-// using UnityEngine;
-// [CreateAssetMenu(fileName = "item", menuName = "inventory/item")]
-// public class Item : ScriptableObject
-// {
-//     public int id;
-//     public string itemName;
-//     public int value;
-//     public Sprite imgage;
-// }
+using UnityEngine;
+[CreateAssetMenu(fileName = "item", menuName = "inventory/item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public int value;
+    public Sprite imgage;
+}
--- Assets/Danh/InventoryAssets/ItemPickup.cs
+++ Assets/Danh/InventoryAssets/ItemPickup.cs
@@ -1,39 +1,39 @@
-// using UnityEngine;
-// using System.Collections.Generic;
-// using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
-// public class ItemPickup : MonoBehaviour
-// {
-//     public Item item;
-//     private bool canPickUp = false;
+public class ItemPickup : MonoBehaviour
+{
+    public Item item;
+    private bool canPickUp = false;
 
-//     void Update()
-//     {
-//         if (canPickUp && Input.GetKeyDown(KeyCode.R))
-//         {
-//             PickUp();
-//         }
-//     }
+    void Update()
+    {
+        if (canPickUp && Input.GetKeyDown(KeyCode.R))
+        {
+            PickUp();
+        }
+    }
 
-//     void PickUp()
-//     {
-//         InventoryManager.Instance.Add(item);
-//         Destroy(gameObject);
-//     }
+    void PickUp()
+    {
+        InventoryManager.Instance.Add(item);
+        Destroy(gameObject);
+    }
 
-//     private void OnTriggerEnter2D(Collider2D collision)
-//     {
-//         if (collision.CompareTag("Player"))
-//         {
-//             canPickUp = true;
-//         }
-//     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canPickUp = true;
+        }
+    }
 
-//     private void OnTriggerExit2D(Collider2D collision)
-//     {
-//         if (collision.CompareTag("Player"))
-//         {
-//             canPickUp = false;
-//         }
-//     }
-// }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canPickUp = false;
+        }
+    }
+}
